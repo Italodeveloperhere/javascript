3a0cd93472dbf800c677fb284a829293aa4cf88e
pservicos <pservicos@WTCHOTEL.COM.BR>
2025-10-31 10:29:07 -0300

    introduction javascript

--- introduction/script01.js
+++ introduction/script01.js
@@ -21,3 +21,18 @@
 // SpiderMonkey – no Firefox.
 // …Existem outros codinomes como “Chakra” para o IE, 
 // “JavaScriptCore”, “Nitro” e “SquirrelFish” para o Safari.
+
+// _______________________________________
+/* Linguagens “sobre” JavaScript */
+// CoffeeScript: é um "açúcar sintático" para JavaScript. Ele introduz uma sintaxe mais curta, permitindo-nos escrever um código mais claro e preciso. Normalmente, os desenvolvedores Ruby gostam dele.
+// TypeScript: concentra-se em adicionar "tipagem de dados estrita" para simplificar o desenvolvimento e o suporte de sistemas complexos. Ele é desenvolvido pela Microsoft.
+// Flow: também adiciona tipagem de dados, mas de uma maneira diferente. Desenvolvido pelo Facebook.
+// Dart: é uma linguagem independente que possui seu próprio mecanismo e funciona em ambientes que não são navegadores (como aplicativos móveis), mas também pode ser transpilada para JavaScript. Desenvolvida pelo Google.
+// Brython: é um transpilador de Python para JavaScript que permite escrever aplicações em Python puro, sem JavaScript.
+// Kotlin: é uma linguagem de programação moderna, concisa e segura, que pode ser executada tanto no navegador quanto no Node.js.
+
+// _______________________________________
+/* Resumo */
+// O JavaScript foi inicialmente criado como uma linguagem exclusiva para navegadores, mas agora também é usado em muitos outros ambientes.
+// Hoje, o JavaScript ocupa uma posição única como a linguagem de navegador mais amplamente adotada, totalmente integrada ao HTML/CSS.
+// Existem muitas linguagens que são "transpiladas" para JavaScript e oferecem certos recursos. Recomenda-se dar uma olhada nelas, ainda que brevemente, depois de dominar o JavaScript.

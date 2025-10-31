0114edd379e9349e5b412cfc8c21b4b6c9f9baa3
pservicos <pservicos@WTCHOTEL.COM.BR>
2025-10-31 09:06:27 -0300

    O JavaScript foi inicialmente criado como uma linguagem exclusiva para navegadores, mas agora também é usado em muitos outros ambientes.

---
+++ introduction/script01.js
@@ -0,0 +1,23 @@
+/* O que é JavaScript? */
+// O JavaScript foi criado inicialmente para "dar vida às páginas da web".
+// Os programas nessa linguagem são chamados de scripts . Eles podem ser 
+// escritos diretamente no HTML de uma página da web e 
+// executados automaticamente quando a página é carregada.
+// Os scripts são fornecidos e executados como texto simples. 
+// Não necessitam de preparação ou compilação especial para serem executados.
+
+// _______________________________________
+/* O que o JavaScript no navegador pode fazer? */
+// Adicione novo HTML à página, altere o conteúdo existente, modifique os estilos.
+// Reaja às ações do usuário, execute ações com cliques do mouse, movimentos do ponteiro e pressionamentos de teclas.
+// Enviar solicitações pela rede para servidores remotos, baixar e carregar arquivos (tecnologias conhecidas como AJAX e COMET ).
+// Obter e definir cookies, fazer perguntas ao visitante, exibir mensagens.
+// Armazene os dados no lado do cliente (“armazenamento local”).
+
+// _______________________________________
+/* MOTORES */
+// Diferentes motores têm diferentes "nomes de código". 
+// V8 – no Chrome, Opera e Edge.
+// SpiderMonkey – no Firefox.
+// …Existem outros codinomes como “Chakra” para o IE, 
+// “JavaScriptCore”, “Nitro” e “SquirrelFish” para o Safari.

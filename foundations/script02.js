1606c57a798ab17f51b90a0c85d56e428fb39b8e
italodeveloperhere <italodeveloper.here@gmail.com>
2025-10-31 12:44:32 -0300

    estrutura do codigo

---
+++ foundations/script02.js
@@ -0,0 +1,8 @@
+// Estrutura do código
+
+// Declarações
+console.log("Hello"); //final ponto e virgula
+console.log("my javascript"); //nova instrução, nova linha
+
+var number = 200;
+console.log(number);
